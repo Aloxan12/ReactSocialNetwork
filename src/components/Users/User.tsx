db1c4c3a0a2b45d042c357be76d97e37f9d7fc9b
Aloxan12 <kvakvaaak@mail.ru>
2021-05-22 11:06:32 +0300

    added User Component

---
+++ src/components/Users/User.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import userPhoto from "../../assets/images/user.png";
+import styles from "./users.module.css";
+import {UserType} from "../../redux/users-reducer";
+
+type UserPropsType = {
+    user: UserType
+    follow: (userID: number) => void
+    unfollow: (userID: number) => void
+    followingIsProgress: Array<number>
+}
+
+export const User = React.memo((props: UserPropsType) => {
+    let u = props.user;
+    return (
+        <div>
+
+            <div>
+                    <span>
+                        <div>
+                           <NavLink to={'/profile/' + u.id}>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                     className={styles.UsersPhoto}/>
+                           </NavLink>
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button disabled={props.followingIsProgress.some(id => id === u.id)}
+                                          onClick={() => {
+                                              props.unfollow(u.id)
+                                          }}>Unfollow</button>
+                                : <button disabled={props.followingIsProgress.some(id => id === u.id)}
+                                          onClick={() => {
+                                              props.follow(u.id)
+                                          }}>Follow</button>}
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
+                    </span>
+            </div>
+
+        </div>
+    )
+})
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import userPhoto from "../../assets/images/user.png";
 import { Paginator } from "../Common/Paginator/Paginator";
-import styles from "./users.module.css";
 import {UsersPropsType} from "./UsersContainer";
+import {User} from "./User";
 
 
 export const Users = React.memo ((props: UsersPropsType) => {
@@ -16,45 +14,17 @@ export const Users = React.memo ((props: UsersPropsType) => {
     return (
         <div>
             {
-                props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                           <NavLink to={'/profile/' + u.id}>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                                     className={styles.UsersPhoto}/>
-                           </NavLink>
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button disabled={props.followingIsProgress.some(id=> id === u.id)}
-                                          onClick={() =>{props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                : <button disabled={props.followingIsProgress.some(id=> id === u.id)}
-                                          onClick={() =>{props.follow(u.id)
-                                }}>Follow</button>}
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{"u.location.country"}</div>
-                            <div>{"u.location.city"}</div>
-                        </span>
-                    </span>
-                </div>)
+                props.users.map(u => <User followingIsProgress={props.followingIsProgress}
+                                           unfollow={props.unfollow}
+                                           follow={props.follow}
+                                           user={u}
+                                           key={u.id}
+                />)
             }
-            <Paginator currentPage={props.currentPage} onPageChanged={props.onPageChanged} pageSize={props.pageSize} totalUsersCounts={props.totalUsersCounts}  />
-            {/*<div className={styles.pageBlock}>*/}
-            {/*    {pages.map(p => {*/}
-            {/*        return <span className={props.currentPage === p ? styles.selectedPage : ""}*/}
-            {/*                     onClick={(e) => {*/}
-            {/*                         props.onPageChanged(p)*/}
-            {/*                     }}>{p}</span>*/}
-            {/*    })}*/}
-            {/*</div>*/}
+            <Paginator currentPage={props.currentPage}
+                       onPageChanged={props.onPageChanged}
+                       pageSize={props.pageSize}
+                       totalUsersCounts={props.totalUsersCounts}  />
         </div>
     )
 })
