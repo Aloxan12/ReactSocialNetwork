62af9852e36d2c34b8d5d456a4dedd860767ee78
Aloxan12 <kvakvaaak@mail.ru>
2021-05-26 11:15:50 +0300

    added React.lazy and React.Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,20 @@ import News from './components/News/News';
 import Setting from './components/Setting/Setting';
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import store, {RootReduxStateType} from "./redux/redux-store";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializedApp} from "./redux/app-reducer";
 import Preloader from "./components/Common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
 
+const DialogsContainer = React.lazy(()=>import ("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(()=>import ("./components/Profile/ProfileContainer"));
+
+const SuspendedDialogs = withSuspense(DialogsContainer)
+const SuspendedProfile = withSuspense(ProfileContainer)
 
 class App extends React.Component<mapStateToPropsType & mapDispatchToPropsType> {
   componentDidMount() {
@@ -31,8 +35,8 @@ class App extends React.Component<mapStateToPropsType & mapDispatchToPropsType> 
         <HeaderContainer />
         <Navbar state={this.props.stateForNavbar}/>
         <div className="app-wrapper-content">
-          <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-          <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+          <Route path='/profile/:userId?' render={() => <SuspendedProfile/>}/>
+          <Route path='/dialogs' render={() => <SuspendedDialogs/>}/>
           <Route path='/users' render={() => <UsersContainer/>}/>
           <Route path='/login' render={() => <Login/>}/>
           <Route path='/music' render={() => <Music/>}/>
