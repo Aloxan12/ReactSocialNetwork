e3f5057b362bb1aea933d653959cfb4ac912160c
Aloxan12 <kvakvaaak@mail.ru>
2022-05-10 11:57:44 +0300

    slider

--- src/components/SliderPage/SliderPage.tsx
+++ src/components/SliderPage/SliderPage.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from './SliderPage.module.css'
 import {dataSlider} from "./DataSlider";
 import img from './Images/img.png'
+import img_1 from './Images/img_1.png'
+import img_2 from './Images/img_2.png'
+import img_3 from './Images/img_3.png'
+import img_4 from './Images/img_4.png'
 
 export const SliderPage =()=>{
     return(
@@ -9,7 +13,7 @@ export const SliderPage =()=>{
             <img src={img}/>
             {dataSlider.map((item, index) =>{
                 return(
-                    <div key={item.id}>
+                    <div key={item.id} >
                         <img src={require(`./Images/img_${index + 1}.png`)}/>
                     </div>
                 )
